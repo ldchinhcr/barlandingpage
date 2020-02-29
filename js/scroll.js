0008c9c65b61e002f163a7a2e1239aa1bd0cd209
ldchinhcr <58549420+ldchinhcr@users.noreply.github.com>
2020-02-29 18:09:08 +0700

    changed

--- js/scroll.js
+++ js/scroll.js
@@ -5,3 +5,11 @@ $(document).ready(function() {
         }, 700);
     });
 });
+
+$(document).ready(function() {
+    $(".scroller-menu").click(function() {
+        $('html, body').animate({
+            scrollTop: $(".nhan-menu-title").offset().top
+        }, 700);
+    });
+});
